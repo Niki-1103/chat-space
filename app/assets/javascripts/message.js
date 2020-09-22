e2188d87afc8b3a725d3a5f823fbb53a5449b41c
Hitomi-y <yhitomi222@gmail.com>
2020-09-22 23:35:30 +0900

    incremental-research update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -36,7 +36,7 @@ $(function(){
   };
 }
 
-  $('Form').on('submit', function(e) {
+  $('.form').on('submit', function(e) {
     e.preventDefault()
     let formData = new FormData(this);
     let url = $(this).attr('action');
